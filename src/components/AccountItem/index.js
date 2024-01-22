18d30a9ff649a3f81de62f0bacf5bc6e3c9be600
DucThanh <phoducthanh@gmail.com>
2024-01-22 18:00:16 +0700

    Build UI Header #3

---
+++ src/components/AccountItem/index.js
@@ -0,0 +1,27 @@
+import classNames from 'classnames/bind';
+import styles from './AccountItem.module.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
+
+const cx = classNames.bind(styles);
+
+function AccountItem() {
+    return (
+        <div className={cx('wrapper')}>
+            <img
+                className={cx('avatar')}
+                src="https://p16-sign-useast2a.tiktokcdn.com/tos-useast2a-avt-0068-euttp/1f6dbdd8b0f0f7728dd3641285f4ecc5~c5_300x300.webp?lk3s=a5d48078&x-expires=1706090400&x-signature=6JUPEptDNq4A5kDA%2Far514RN4s0%3D"
+                alt="avatar"
+            />
+            <div className={cx('info')}>
+                <h4 className={cx('name')}>
+                    <span>Nguyen Van A</span>
+                    <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                </h4>
+                <span className={cx('username')}>nguyenvana</span>
+            </div>
+        </div>
+    );
+}
+
+export default AccountItem;
