db5121921a763151825b533f47898c8255ffea66
DucThanh <phoducthanh@gmail.com>
2024-01-29 09:47:58 +0700

    Build UI Sidebar #2.1

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,50 @@
+import { GoOnTopIcon } from '@/assests/icons';
+import { useEffect, useState } from 'react';
+
 function Home() {
-    return <h2>Home page</h2>;
+    const [showGoOnTop, setShowGoOnTop] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY >= 100) {
+                // Show
+                setShowGoOnTop(true);
+            } else {
+                // Hide
+                setShowGoOnTop(false);
+            }
+
+            // setShowGoOnTop(window.scrollY >= 200)
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        // cleanup function
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <div>
+            <h2 style={{ height: 2000 }}>Home page</h2>
+            {showGoOnTop && (
+                <button
+                    style={{
+                        backgroundColor: 'red',
+                        borderRadius: '50%',
+                        width: 32,
+                        height: 32,
+                        position: 'fixed',
+                        right: 20,
+                        bottom: 20,
+                    }}
+                >
+                    <GoOnTopIcon width="1.6rem" height="1.6rem" />
+                </button>
+            )}
+        </div>
+    );
 }
 
 export default Home;
